d2527170b7e51dccb47fc5a280242835653e9cb4
anton <blazhennovanton@yandex.ru>
2024-01-10 14:45:00 +0300

    12-2-14 done

--- Lesson_12-2/12-2-14.py
+++ Lesson_12-2/12-2-14.py
@@ -1,9 +1,31 @@
 import random
+import string
 
 def generate_password(length):
-    pass
+    exclude = 'lI1oO0'
+
+    letters = [[x for x in string.ascii_uppercase if x not in exclude],
+               [x for x in string.ascii_lowercase if x not in exclude],
+               [x for x in string.digits if x not in exclude]]
+
+    pasw = [random.choice(letters[0]),
+            random.choice(letters[1]),
+            random.choice(letters[2])]
+
+    for i in range(length - 3):
+        flag = True
+        while flag == True:
+            x = random.choice(string.ascii_letters + string.digits)
+            if x not in exclude:
+                pasw.append(x)
+                flag = False
+    random.shuffle(pasw)
+    print(*pasw, sep='')
 
 def generate_passwords(count, length):
-    pass
+    for i in range(count):
+        generate_password(length)
 
 n, m = int(input()), int(input())
+
+generate_passwords(n, m)
