9fb44fb61b342b262ff05cc37ba53c39d8a99b67
Entir <Blazhennovanton@yandex.ru>
2024-01-17 21:44:50 +0300

    lessons 14-1-8

---
+++ Py_Lessoons/Lessons_14-1/14-1-8.py
@@ -0,0 +1,15 @@
+import turtle
+
+
+def rmb(side):
+    turtle.forward(side)
+    turtle.left(120)
+    turtle.forward(side)
+    turtle.left(60)
+    turtle.forward(side)
+    turtle.left(120)
+    turtle.forward(side)
+    turtle.left(60)
+
+
+rmb(100)
