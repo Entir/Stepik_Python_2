d5ab37e75b333fffad83f52bc6ca6c90ab8c9ae1
Entir <Blazhennovanton@yandex.ru>
2024-01-17 21:18:08 +0300

    lessons 14-1-4

---
+++ Py_Lessoons/Lessons_14-1/14-1-4.py
@@ -0,0 +1,33 @@
+import turtle
+
+
+def square(side):
+    turtle.left(45)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+
+    turtle.left(60)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+
+    turtle.left(150)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+
+
+square(100)
